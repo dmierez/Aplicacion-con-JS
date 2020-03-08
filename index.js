305631a701fb58cb34f6d9c9920fd0d4977b1177
Diego Mierez <dmierez@gmail.com>
2020-03-08 02:02:07 -0300

    Versión final de la app

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ class Nueva_Interfaz {
             <div class="card text-left mb-4 pl-2">
                 <div class="card-body">
                     Soy <strong>${_NuevoEventoProblema.Línea}</strong>
-                    <strong>- </strong> ${_NuevoEventoProblema.Evento}        
+                    <strong>- </strong> ${_NuevoEventoProblema.Evento}
+                    <br>
+                    <a href="#" class="btn btn-danger ml-4 mt-4" name="delete">Borrar</a>        
                 </div>
             </div>
         `;
@@ -24,12 +26,23 @@ class Nueva_Interfaz {
         document.getElementById('evento-form').reset();
     }
 
-    EliminarEvento() {
-
+    EliminarEvento(element) {
+        if(element.name === 'delete') {
+            element.parentElement.parentElement.parentElement.remove();
+            this.MostrarMensaje('Eliminaste el proyecto. Puede cargar uno nuevo!', 'danger')
+        }
     }
 
-    MostrarMensaje() {
-
+    MostrarMensaje(msj, cssClass) {
+        const div = document.createElement('div');
+        div.className = `mt-2 alert alert-${cssClass}`;
+        div.appendChild(document.createTextNode(msj))
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#App');
+        container.insertBefore(div, app);
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        }, 2500)
     }
 
 }
@@ -43,7 +56,20 @@ document.getElementById('evento-form')
         
         const NuevoEventoProblema = new EventoProblema(LíneaDeTrabajo, RegistrarEvento)
 
+
         const ListadoDeEventos = new Nueva_Interfaz();
+        
+        if(LíneaDeTrabajo === '' || RegistrarEvento === '') {
+            return ListadoDeEventos.MostrarMensaje('Termina de completar lo que falta', 'info')
+        }
         ListadoDeEventos.agregarEvento(NuevoEventoProblema)
         ListadoDeEventos.resetearFormulario();
+        ListadoDeEventos.MostrarMensaje('Felicitaciones! Haz logrado describir tu proyecto en un Twitter Pitch :)', 'success')
 })
+
+document.getElementById('listar-pitch')
+    .addEventListener('click', function (e) {
+        const ListadoDeEventos = new Nueva_Interfaz();
+        ListadoDeEventos.EliminarEvento(e.target);
+
+    })
